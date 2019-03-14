e336c67ffa0923d57cfa313037d972dc7703df2b
Marcelo Antonio e Silva Filho <marcelo.filho@elotech.com.br>
2019-03-14 13:47:56 -0300

    aula-5-typescript

---
+++ aulaP5/AulaPratica5.js
@@ -0,0 +1,11 @@
+var obj = {
+    first_name: 'Marcelo',
+    last_name: 'Silva'
+};
+var variable = 4;
+variable = {};
+variable = "string";
+variable = false;
+var myArray = [2, 'string', false, { name: 'marcelo' }];
+var myArray2 = [2, 'string', false, { name: 'marcelo' }];
+myArray.push(1);
---
+++ aulaP5/AulaPratica5.ts
@@ -0,0 +1,17 @@
+let obj: object = {
+    first_name: 'Marcelo',
+    last_name: 'Silva'
+};
+
+let variable: any = 4;
+
+variable = {};
+
+variable = "string"
+
+variable = false; 
+
+let myArray:Array<any> = [2, 'string', false, {name: 'marcelo'}];
+let myArray2:any[] = [2, 'string', false, {name: 'marcelo'}];
+
+myArray.push(1);
